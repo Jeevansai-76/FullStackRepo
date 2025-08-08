8f8b4a174170bd975d040227de08739f8b44b513
Jeevansai-76 <jeevansai926@gmail.com>
2025-08-09 02:46:39 +0530

    Subtraction func added to arithmetic.java

--- Proj1/src/Arithmetic.java
+++ Proj1/src/Arithmetic.java
@@ -5,9 +5,14 @@ public class Arithmetic {
 		System.out.println(a+b);
 	}
 	
+	public void sub(int a,int b) {
+		System.out.println(a-b);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Arithmetic.main()");
 		new Arithmetic().sum(1,2);
+		new Arithmetic().sub(5, 1);
 	}
 
 }
