5e8cda749596d62cdaca8554a56838937dcfd598
Jeevansai-76 <jeevansai926@gmail.com>
2025-08-10 23:30:25 +0530

    Subtraction method added

--- GitProj-01/src/main/java/com/nt/main/Arithmetic.java
+++ GitProj-01/src/main/java/com/nt/main/Arithmetic.java
@@ -6,10 +6,15 @@ public class Arithmetic {
 		System.out.println(a+b);
 	}
 	
+	public void sub(int a,int b) {
+		System.out.println(a-b);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Arithmetic.main()");
 		Arithmetic ath = new Arithmetic();
 		ath.sum(10, 20);
+		ath.sub(20, 10);
 	}
 
 }
