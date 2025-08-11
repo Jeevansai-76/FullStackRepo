29428ff4add24000904404270e574c4e7314fdd8
Jeevansai-76 <jeevansai926@gmail.com>
2025-08-12 02:21:07 +0530

    ....

--- GitProj-01/src/main/java/com/nt/service/Payment.java
+++ GitProj-01/src/main/java/com/nt/service/Payment.java
@@ -4,4 +4,7 @@ public class Payment {
 	public String PaymentThroughPhonepe() {
 		return "Payment is done for the processing through Phonepe";
 	}
+	public String PaymentThroughPaytm() {
+		return "Payment is done for the processing through Paytm";
+	}
 }
